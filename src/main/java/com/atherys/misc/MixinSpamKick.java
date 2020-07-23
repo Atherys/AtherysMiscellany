cce867ab9f2ad8a0d349a133f57ec5ae7b4cd62d
CodeStripper <brentgerstenschlager@gmail.com>
2020-07-23 19:09:07 -0400

    made some gradle changes, added logic to mixin class.

--- src/main/java/com/atherys/misc/MixinSpamKick.java
+++ src/main/java/com/atherys/misc/MixinSpamKick.java
@@ -1,5 +1,21 @@
 package com.atherys.misc;
 
+import net.minecraft.network.NetHandlerPlayServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(NetHandlerPlayServer.class)
 public class MixinSpamKick {
+
+    @Shadow
+    private int chatSpamThresholdCount;
+
+    @Inject(method = "processChatMessage", at = @At("HEAD"))
+    private void checkMessageCount(CallbackInfo info) {
+        this.chatSpamThresholdCount =- 20;
+    }
 
 }
